1e581d570ab16a57d38068aa1c810308d8c5a4b2
Tom <55004481+TomDuncan44221@users.noreply.github.com>
2021-07-08 22:21:53 +0100

    Update component_getstarted.jsx

--- src/component_getstarted.jsx
+++ src/component_getstarted.jsx
@@ -88,7 +88,7 @@ export default function Home() {
       <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-          Traduality Language Solutions LLC
+          Traduality Language Solutions Inc
           </Typography>
           <nav>
           <Link variant="button" color="textPrimary" href="/" className={classes.link}>

912adbd920bf60aa40569e3db1579709d23c39ee
Tom <55004481+TomDuncan44221@users.noreply.github.com>
2021-07-08 22:16:07 +0100

    Update component_fizzbuzz.jsx

--- src/component_fizzbuzz.jsx
+++ src/component_fizzbuzz.jsx
@@ -84,7 +84,7 @@ export default function FizzBuzz() {
       <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-          Traduality Language Solutions LLC
+          Traduality Language Solutions Inc
           </Typography>
           <nav>
           <Link variant="button" color="textPrimary" href="/" className={classes.link}>
